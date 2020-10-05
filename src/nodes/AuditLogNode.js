71ab6f5a19f6bbefb664eaf2d5cd467e43925780
nagahysa <guemmoudrayane03@gmail.com>
2020-10-05 11:38:45 +0200

    add 2 node

---
+++ src/nodes/AuditLogNode.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class AuditLogNode {
+    constructor(client, guildID) {
+        this.client = client;
+        this.guildID = guildID;
+    };
+
+    async getAuditLog(id = this.guildID, options) {
+        return await this.client.instance.get(`/guilds/${id}/audit-logs`, {params: options});
+    };
+};
